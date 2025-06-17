853f3cfaa60cf613c2961d99734eac3605fc24ef
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-06-17 19:29:12 -0300

    mischief: done

--- python/viagem.py
+++ python/viagem.py
@@ -1,6 +1,11 @@
 dist = float(input("Digite a distância em km: "))
-consumo = float(input("Qual o consumo do carro em km/l"))
-custo = float(input("Qual o preço da gasolina?"))
+consumo = float(input("Qual o consumo do carro em km/l? "))
+precogas = float(input("Qual o preço da gasolina? "))
+litros = 0.0
+dinheiros = 0.0
 
-print(f"Serão necessários {litros}l")
-print(f"A viagem vai custar{}")
+litros = dist/consumo
+dinheiros = precogas * litros
+
+print(f"Serão necessários {litros}l para fazer a viagem")
+print(f"A viagem vai custar R${dinheiros}")
